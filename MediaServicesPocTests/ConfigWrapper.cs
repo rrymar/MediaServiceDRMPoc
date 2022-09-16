eed6970389306ad8b407666be636a8be2d7604ca
RuslanRymar <rrimar@idiq.com>
2022-09-16 11:26:02 +0300

    adding api tests

---
+++ MediaServicesPocTests/ConfigWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace MediaServicesPocTests;
+
+public class ConfigWrapper
+{
+    private readonly IConfiguration config;
+
+    public ConfigWrapper(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public string SubscriptionId => config["SubscriptionId"];
+
+    public string ResourceGroup => config["ResourceGroup"];
+
+    public string AccountName => config["AccountName"];
+
+    public string AadTenantId => config["AadTenantId"];
+
+    public string AadClientId => config["AadClientId"];
+
+    public string AadSecret => config["AadSecret"];
+
+    public Uri ArmAadAudience => new Uri(config["ArmAadAudience"]);
+
+    public Uri AadEndpoint => new Uri(config["AadEndpoint"]);
+
+    public Uri ArmEndpoint => new Uri(config["ArmEndpoint"]);
+
+    public string EventHubConnectionString => config["EventHubConnectionString"];
+
+    public string EventHubName => config["EventHubName"];
+
+    public string StorageContainerName => config["StorageContainerName"];
+
+    public string StorageAccountName => config["StorageAccountName"];
+
+    public string StorageAccountKey => config["StorageAccountKey"];
+
+    public string SymmetricKey => config["SymmetricKey"];
+
+    public string AskHex => config["AskHex"];
+
+    public string FairPlayPfxPath => config["FairPlayPfxPath"];
+
+    public string FairPlayPfxPassword => config["FairPlayPfxPassword"];
+}
---
+++ MediaServicesPocTests/MediaApiTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Azure.Management.Media;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.Rest;
+using Xunit;
+
+namespace MediaServicesPocTests;
+
+public class MediaApiTests
+{
+    private readonly ConfigWrapper config;
+    public MediaApiTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+
+        config = new ConfigWrapper(builder.Build());
+    }
+    
+    public const string TokenType = "Bearer";
+    
+    public static async Task<IAzureMediaServicesClient> CreateMediaServicesClientAsync(ConfigWrapper config)
+    {
+        var scopes = new[] { config.ArmAadAudience + "/.default" };
+
+        var app = ConfidentialClientApplicationBuilder.Create(config.AadClientId)
+            .WithClientSecret(config.AadSecret)
+            .WithAuthority(AzureCloudInstance.AzurePublic, config.AadTenantId)
+            .Build();
+
+        var authResult = await app.AcquireTokenForClient(scopes)
+            .ExecuteAsync()
+            .ConfigureAwait(false);
+
+        var credentials = new TokenCredentials(authResult.AccessToken, TokenType);
+
+        return new AzureMediaServicesClient(config.ArmEndpoint, credentials)
+        {
+            SubscriptionId = config.SubscriptionId,
+        };
+    }
+
+    [Fact]
+    public async Task ItReturnAssetsList()
+    {
+        var client = await CreateMediaServicesClientAsync(config);
+        var assets = await client.Assets.ListAsync(config.ResourceGroup, config.AccountName);
+
+        assets.Count().Should().NotBe(0);
+
+    }
+    
+}
